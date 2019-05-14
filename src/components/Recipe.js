b7f7a9f8b958fffb760043bc6e8682a8d41f6341
Ryan Martin <rjmartin73@gmail.com>
2019-05-14 15:12:41 -0600

    VideoEnded

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const API_KEY = 'e45ae5d3784bf0a04da28ddaa0f17e3b';
 
@@ -18,8 +19,24 @@ class Recipe extends React.Component {
 	};
 
 	render() {
-		// console.log(this.state.publisher);
-		return <div>{this.state.activeRecipe.title}</div>;
+
+		const recipe = this.state.activeRecipe;
+
+		return (
+			<div className="container">
+				{this.state.activeRecipe.length !== 0 &&
+					<div className="active-recipe">
+						<img className="active-recipe__img" src={recipe.image_url} alt={recipe.title} />
+						<h3 className="active-recipe__title">{recipe.title}</h3>
+						<h4 className="active-recipe__publisher">
+							Publisher: <span>{recipe.publisher}</span></h4>
+						<p className="active-recipe__website">
+							Website: <span><a href={recipe.publisher_url}>{recipe.publisher_url}</a></span></p>
+						<button className="active-recipe__button">
+						<Link to="/">Return to Search</Link>
+						</button>
+					</div>}
+			</div>)
 	}
 }
 export default Recipe;
